45c0cd3ec1d50e03eb03bddde5863ce108517942
alequoy <alequoy@LEQUOYA7x64.arkadin.lan>
2012-10-16 11:57:57 +0200

    Add controller data_dictionary for all server Add return carriage

--- arkadin/application/controller/data_dictionary.controller.php
+++ arkadin/application/controller/data_dictionary.controller.php
@@ -8,111 +8,173 @@ class data_dictionary extends Controller {
 		
 	}
 
-	function load() {
+	function test_sql_server() {
 
-		echo "Garbage Colletor enabled (défaut) : " . (gc_enabled() ? 'OUI' : 'NON') . "\n";
-		gc_enable();
-		echo "Garbage Colletor enabled (défaut) : " . (gc_enabled() ? 'OUI' : 'NON') . "\n";
-
-		$_MSSQL = Singleton::getInstance("pdo_sqlsrv");
-		$dbh = $_MSSQL->sql_connect("10.102.28.5", "sa", "3fN4uL");
-
-		if (!$dbh)
-		{
-			die("connection impossible");
-		}
-
-		$sql = "SELECT name FROM master..sysdatabases";
-
-		$stmt1 = $dbh->prepare($sql);
-		$stmt1->execute();
 
 		$_SQL = Singleton::getInstance(SQL_DRIVER);
 
-		$i = 0;
-		while ($row1 = $stmt1->fetch()) {
+		$sql = "SELECT * FROM data_dictionary_server where is_valid = 1 order by ip";
+		$res = $_SQL->sql_query($sql);
 
-			unset($base);
-			$_SQL->query = array();
+		while ($ob = $_SQL->sql_fetch_object($res)) {
 
-			echo 'Avant : ' . number_format(memory_get_usage(), 0, '.', ',') . " octets\n";
-			echo "Memoire liberer : " . gc_collect_cycles() . "\n";
-			echo 'Pic : ' . number_format(memory_get_peak_usage(), 0, '.', ',') . " octets\n";
-			echo 'Après : ' . number_format(memory_get_usage(), 0, '.', ',') . " octets\n";
+			echo "Try to connect :".$ob->ip;
+			echo "...";
+			echo str_repeat(" ",15-strlen($ob->ip)+1);
+			$link = @mssql_connect($ob->ip, $ob->login, $ob->password);
 
-			$base['data_dictionary_base']['id_data_dictionary_server'] = 1;
-			$base['data_dictionary_base']['name'] = $row1['name'];
+			
+			
 
-			$i++;
-			echo "traitement du serveur n°" . $i . " : " . $base['data_dictionary_base']['name'] . "\n";
-
-			unset($table);
-			$table['data_dictionary_table']['id_data_dictionary_base'] = $_SQL->sql_save($base);
-
-			if ($table['data_dictionary_table']['id_data_dictionary_base'])
+			if (!$link)
 			{
-				$sql = "use " . $base['data_dictionary_base']['name'];
-				$stmt2 = $dbh->prepare($sql);
-				$stmt2->execute();
+				echo "ERROR (Unable to connect to server: ".$ob->ip.")\n";
+				$data = array();
+				$data['data_dictionary_server']['is_ok'] = 3;
+				$data['data_dictionary_server']['id'] = $ob->id;
 
-				$sql = "SELECT name FROM sysobjects WHERE type='U'";
-				$stmt3 = $dbh->prepare($sql);
-				$stmt3->execute();
+				$ret = $_SQL->sql_save($data);
 
-				while ($row2 = $stmt3->fetch()) {
-					$table['data_dictionary_table']['name'] = $row2['name'];
-
-					unset($field);
-					$field['data_dictionary_field']['id_data_dictionary_table'] = $_SQL->sql_save($table);
-
-					if ($field['data_dictionary_field']['id_data_dictionary_table'])
-					{
-
-						$sql = "SELECT   COLUMN_NAME, ORDINAL_POSITION, COLUMN_DEFAULT,IS_NULLABLE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-                            FROM INFORMATION_SCHEMA.COLUMNS
-                            WHERE TABLE_NAME='" . $table['data_dictionary_table']['name'] . "'";
-						$stmt4 = $dbh->prepare($sql);
-						$stmt4->execute();
-
-						while ($row3 = $stmt4->fetch()) {
-
-							$field['data_dictionary_field']['column_name'] = $row3['COLUMN_NAME'];
-							$field['data_dictionary_field']['ordinal_position'] = $row3['ORDINAL_POSITION'];
-							$field['data_dictionary_field']['column_default'] = $row3['COLUMN_DEFAULT'];
-							$field['data_dictionary_field']['is_nullable'] = $row3['IS_NULLABLE'];
-							$field['data_dictionary_field']['data_type'] = $row3['DATA_TYPE'];
-							$field['data_dictionary_field']['char_max_length'] = $row3['CHARACTER_MAXIMUM_LENGTH'];
-
-							unset($detail);
-							$detail['none']['id_data_dictionary_field'] = $_SQL->sql_save($field);
-
-							if ($detail['none']['id_data_dictionary_field'])
-							{
-								
-							}
-							else
-							{
-								debug($field);
-								debug($_SQL->sql_error());
-								debug($_SQL->query);
-								die("insertion dans data_dictionary_field impossible");
-							}
-						}
-					}
-					else
-					{
-						die("insertion dans data_dictionary_table impossible");
-					}
-
-					unset($stmt4);
-					unset($row3);
+				if (!$ret)
+				{
+					debug($data);
+					debug($_SQL->sql_error());
+					die();
 				}
 			}
 			else
 			{
+				echo "connected successfully\n";
+				$data = array();
+				$data['data_dictionary_server']['is_ok'] = 1;
+				$data['data_dictionary_server']['id'] = $ob->id;
 
-				debug($_SQL->sql_error());
-				die("insertion dans data_dictionary_base impossible");
+				$ret = $_SQL->sql_save($data);
+
+				if (!$ret)
+				{
+					debug($data);
+					debug($_SQL->sql_error());
+					die();
+				}
+			}
+			
+			@mssql_close($link);
+		}
+		
+		exit;
+	}
+
+	function load() {
+
+		$_SQL = Singleton::getInstance(SQL_DRIVER);
+		
+
+
+		$sql = "SELECT * FROM data_dictionary_server where id !=1 and is_valid = 1 and is_ok =1";
+		$res = $_SQL->sql_query($sql);
+
+		while ($ob = $_SQL->sql_fetch_object($res)) {
+
+			$dbh = $_MSSQL->sql_connect($ob->ip, $ob->login, $ob->password);
+
+			if (!$dbh)
+			{
+				die("connection impossible to : " . $ob->ip . "\n");
+			}
+
+			$sql = "SELECT name FROM master..sysdatabases";
+
+			$stmt1 = $dbh->prepare($sql);
+			$stmt1->execute();
+
+
+
+			$i = 0;
+			while ($row1 = $stmt1->fetch()) {
+
+				unset($base);
+				$_SQL->query = array();
+
+				echo 'Avant : ' . number_format(memory_get_usage(), 0, '.', ',') . " octets\n";
+				echo "Memoire liberer : " . gc_collect_cycles() . "\n";
+				echo 'Pic : ' . number_format(memory_get_peak_usage(), 0, '.', ',') . " octets\n";
+				echo 'Après : ' . number_format(memory_get_usage(), 0, '.', ',') . " octets\n";
+
+				$base['data_dictionary_base']['id_data_dictionary_server'] = 1;
+				$base['data_dictionary_base']['name'] = $row1['name'];
+
+				$i++;
+				echo "traitement du serveur n°" . $i . " : " . $base['data_dictionary_base']['name'] . "\n";
+
+				unset($table);
+				$table['data_dictionary_table']['id_data_dictionary_base'] = $_SQL->sql_save($base);
+
+				if ($table['data_dictionary_table']['id_data_dictionary_base'])
+				{
+					$sql = "use " . $base['data_dictionary_base']['name'];
+					$stmt2 = $dbh->prepare($sql);
+					$stmt2->execute();
+
+					$sql = "SELECT name FROM sysobjects WHERE type='U'";
+					$stmt3 = $dbh->prepare($sql);
+					$stmt3->execute();
+
+					while ($row2 = $stmt3->fetch()) {
+						$table['data_dictionary_table']['name'] = $row2['name'];
+
+						unset($field);
+						$field['data_dictionary_field']['id_data_dictionary_table'] = $_SQL->sql_save($table);
+
+						if ($field['data_dictionary_field']['id_data_dictionary_table'])
+						{
+
+							$sql = "SELECT   COLUMN_NAME, ORDINAL_POSITION, COLUMN_DEFAULT,IS_NULLABLE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+                            FROM INFORMATION_SCHEMA.COLUMNS
+                            WHERE TABLE_NAME='" . $table['data_dictionary_table']['name'] . "'";
+							$stmt4 = $dbh->prepare($sql);
+							$stmt4->execute();
+
+							while ($row3 = $stmt4->fetch()) {
+
+								$field['data_dictionary_field']['column_name'] = $row3['COLUMN_NAME'];
+								$field['data_dictionary_field']['ordinal_position'] = $row3['ORDINAL_POSITION'];
+								$field['data_dictionary_field']['column_default'] = $row3['COLUMN_DEFAULT'];
+								$field['data_dictionary_field']['is_nullable'] = $row3['IS_NULLABLE'];
+								$field['data_dictionary_field']['data_type'] = $row3['DATA_TYPE'];
+								$field['data_dictionary_field']['char_max_length'] = $row3['CHARACTER_MAXIMUM_LENGTH'];
+
+								unset($detail);
+								$detail['none']['id_data_dictionary_field'] = $_SQL->sql_save($field);
+
+								if ($detail['none']['id_data_dictionary_field'])
+								{
+									
+								}
+								else
+								{
+									debug($field);
+									debug($_SQL->sql_error());
+									debug($_SQL->query);
+									die("insertion dans data_dictionary_field impossible");
+								}
+							}
+						}
+						else
+						{
+							die("insertion dans data_dictionary_table impossible");
+						}
+
+						unset($stmt4);
+						unset($row3);
+					}
+				}
+				else
+				{
+
+					debug($_SQL->sql_error());
+					die("insertion dans data_dictionary_base impossible");
+				}
 			}
 		}
 		//debug($count);
--- arkadin/application/controller/retour_chariot.controller.php
+++ arkadin/application/controller/retour_chariot.controller.php
@@ -4,12 +4,148 @@ class retour_chariot {
 
 	function index() {
 		
-		
-		
-		
 	}
-	
-	
-	
+
+	function import_fields() {
+		$_SQL = Singleton::getInstance(SQL_DRIVER);
+
+		$tab = file("");
+
+
+		foreach ($tab as $line)
+		{
+			$tab_elem = explode(";", $line);
+
+			$i = 1;
+			foreach ($tab_elem as $elem)
+			{
+
+				if ($i == 1)
+				{
+					$base = explode("_", $elem);
+
+					$nb_elem = count($base) - 1;
+					$base_name = array();
+					for ($j = 0; $j < $nb_elem; $j++)
+					{
+						$base_name[] = $base[$j];
+
+						$try_base = implode("_", $base_name);
+
+						$sql = "SELECT id FROM data_dictionary_base WHERE name= '" . $try_base . "' and ";
+						$res = $_SQL->sql_query($sql);
+
+						if ($_SQL->sql_num_rows($res) === 1)
+						{
+							$ob = $_SQL->sql_fetch_object($res);
+							$id_base = $ob->id;
+
+							$table_name = array();
+							for ($k = $i + 1; $k < $nb_elem; $k++)
+							{
+								$table_name[] = $base[$k];
+							}
+
+							$try_table = implode("_", $table_name);
+
+							$sql = "SELECT id FROM data_dictionary_table WHERE name= '" . $try_table . "' and id_data_dictionary_base = '" . $id_base . "'";
+							$res = $_SQL->sql_query($sql);
+
+							if ($_SQL->sql_num_rows($res) === 1)
+							{
+								$ob = $_SQL->sql_fetch_object($res);
+								$id_table = $ob->id;
+
+								echo "Base : " . $try_base . " - Table : " . $try_table . " => added\n";
+								break;
+							}
+							else
+							{
+								$id_table = 0;
+								echo "Impossible to find the table : " . $try_table . "\n";
+								continue 2;
+							}
+						}
+
+						if ($j + 1 == $nb_elem)
+						{
+							
+						}
+					}
+				}
+				else
+				{
+					
+				}
+
+
+				$i++;
+			}
+		}
+	}
+
+	function import_from_file() {
+		
+		$_SQL = Singleton::getInstance(SQL_DRIVER);
+		$data = file("RetourChariot.csv");
+
+		foreach ($data as $line)
+		{
+			$tab_elem = explode(";", $line);
+
+
+			$i = 1;
+			foreach ($tab_elem as $elem)
+			{
+				if ($i == 1)
+				{
+					$base_table = array();
+		
+					$base_table['retour_chariot_base_table']['base_table'] = $elem;
+					
+					$id_base_table = $_SQL->sql_save($base_table);
+					
+					if (! $id_base_table)
+					{
+						debug($_SQL->sql_error());
+						debug($base_table);
+						echo "impossible to save base_table\n";
+						break;
+					}
+				}
+				else
+				{
+					
+					if (empty($elem))
+					{
+						break;
+					}
+					
+					
+					$data = array();
+		
+					$data['retour_chariot_field']['id_retour_chariot_base_table'] = $id_base_table;
+					$data['retour_chariot_field']['field'] = $elem;
+					
+					$id_field = $_SQL->sql_save($data);
+					
+					
+					if (! $id_field)
+					{
+						debug($_SQL->sql_error());
+						debug($data);
+						echo "impossible to save base_table\n";
+						break;
+					}
+					else
+					{
+						echo "Field added : ".$elem."\n";
+					}
+				}
+
+				$i++;
+			}
+		}
+	}
 
 }
